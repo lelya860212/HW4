35b92455ecaa3da177828bb4b55eed63ee517431
Olga <lelya860212@gmail.com>
2023-09-30 19:52:30 +0300

    1 commit

---
+++ 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Summa(int A)
+{
+int Res=A;
+int Summ=0;
+while (Res>0)
+{
+    int Dig=Res%10;
+Summ=Summ+Dig;
+    Res=Res/10;
+}
+Console.WriteLine(Summ);
+return Summ;
+}
+Summa(452);
